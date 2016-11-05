de787940e410686e4504bdc6cf8b587f3a8f8f0f
Brandon Truong <me@brandontruong.com>
2016-11-05 16:43:06 -0700

    Basic working bot with boiler message response

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import * as messenger from './messenger/index';
+import bodyParser from 'body-parser';
+
+let app = express();
+
+app.set('port', (process.env.PORT || 3000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/messenger/webhook', messenger.verifyMessenger);
+app.post('/messenger/webhook', messenger.handleMessages);
+
+app.listen(app.get('port'), () => {
+  console.log('Server listening at port', app.get('port'));
+});
+
